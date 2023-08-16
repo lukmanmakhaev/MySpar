0d4d6b93685616adb3c4318def37c10416a3e1d6
Lukman <lukmanmakhaev@gmail.com>
2023-08-16 18:10:21 +0300

    Initial Commit

---
+++ HHTestApp/HHTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HHTestAppApp.swift
+//  HHTestApp
+//
+//  Created by Lukman Makhaev on 16.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct HHTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
